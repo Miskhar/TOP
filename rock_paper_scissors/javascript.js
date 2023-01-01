5abe77681493986683f5197bc0d0ba51abc821e7
KT <Rawrinc@gmail.com>
2022-12-31 16:53:31 -0800

    add functionality for all weapon selections, add tie message

--- rock_paper_scissors/javascript.js
+++ rock_paper_scissors/javascript.js
@@ -1,6 +1,9 @@
 let playerScore = 0;
 let cpuScore = 0;
 let cpuRand = 0;
+let playerSelection = null;
+let whoWins = null;
+let tempWeap = null;
 // Convert the strings to a numbers
 cpuRand = Number(cpuRand);
 playerScore = Number(playerScore);
@@ -28,34 +31,75 @@ function cpuWeaponSelect() {
     return (selection);
 }
 
-function scoreUpdate(playerScore, cpuScore) {
+function scoreUpdate(playerScore, cpuScore, cpuSelection) {
+    // Update the player score and enemy score
     document.getElementById("pScore").innerHTML = `Player Score: ${playerScore}`;
     document.getElementById("cScore").innerHTML = `Enemy Score: ${cpuScore}`;
+    // Display what weapon the enemy chose (Practice capitalizing as well, why not?)
+    document.getElementById("enemyChoice").innerHTML = `Enemy Chose: ${cpuSelection[0].toUpperCase() + cpuSelection.substring(1)}`;
+}
+
+function roundLogic(playerSelection, cpuSelection) {
+    if (playerSelection.toLowerCase() === "rock") {
+        if (cpuSelection.toLowerCase() === "rock") {
+            return whoWins = "tie";
+        } else if (cpuSelection.toLowerCase() === "paper") {
+            return whoWins = "cpu";
+        } else {
+            return whoWins = "player";
+        }
+    }
+    if (playerSelection.toLowerCase() === "paper") {
+        if (cpuSelection.toLowerCase() === "rock") {
+            return whoWins = "player";
+        } else if (cpuSelection.toLowerCase() === "paper") {
+            return whoWins = "tie";
+        } else {
+            return whoWins = "cpu";
+        }
+    }
+    if (playerSelection.toLowerCase() === "scissors") {
+        if (cpuSelection.toLowerCase() === "rock") {
+            return whoWins = "cpu";
+        } else if (cpuSelection.toLowerCase() === "paper") {
+            return whoWins = "player";
+        } else {
+            return whoWins = "tie";
+        }
+    } 
+    // Needs to return "player" or "cpu"
+}
+
+
+function playRound(playerSelection) {
+    let cpuSelection = cpuWeaponSelect();
+    whoWins = roundLogic(playerSelection, cpuSelection);
+    if (whoWins.toLowerCase() === "player") {
+        ++playerScore;
+    } else if (whoWins.toLowerCase() === "cpu") {
+        ++cpuScore;
+    }
+    if (whoWins.toLowerCase() === "tie") {
+        tempWeap = cpuSelection;
+        cpuSelection = `Enemy chose ${tempWeap}, so it is a DRAW!`;
+    }
+    scoreUpdate(playerScore, cpuScore, cpuSelection);
 }
 
 function playerSelectedRock() {
-    let cpuSelection = cpuWeaponSelect();
-    let whoWins = null;
-    if (cpuSelection.toLowerCase() === "rock") {
-        whoWins = "tie";
-    } else if (cpuSelection.toLowerCase() === "paper") {
-        whoWins = "cpu";
-    } else {
-        whoWins = "player";
-    }
-    // alert("The winner is: " + whoWins);
-    if (whoWins.toLowerCase() === "player") {
-        ++playerScore;
-    } else if (whoWins.toLocaleLowerCase() === "cpu") {
-        ++cpuScore;
-    }
-    scoreUpdate(playerScore, cpuScore);
+    playerSelection = "rock";
+    console.log("player selected rock");
+    playRound(playerSelection);
 }
 
 function playerSelectedPaper() {
-    
+    playerSelection = "paper";
+    console.log("player selected paper");
+    playRound(playerSelection);
 }
 
 function playerSelectedScissors() {
-    
+    playerSelection = "scissors";
+    console.log("player selected scissors");
+    playRound(playerSelection);
 }
